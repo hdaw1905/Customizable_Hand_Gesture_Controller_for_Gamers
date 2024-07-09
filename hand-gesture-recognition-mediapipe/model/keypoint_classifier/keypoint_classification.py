48f11fa9280a8a240be72437b678b9fcde632ec7
hdaw1905 <hesham.eina@gmail.com>
2024-07-09 18:28:23 +0300

    2nd Commit

---
+++ hand-gesture-recognition-mediapipe/model/keypoint_classifier/keypoint_classification.py
@@ -0,0 +1,119 @@
+import csv
+import absl.logging
+absl.logging._initialize()
+import numpy as np
+import tensorflow as tf
+from sklearn.model_selection import train_test_split
+import os
+os.environ["TF_USE_LEGACY_KERAS"] = "1"
+
+RANDOM_SEED = 42
+dataset = "C:/Users/hesha/OneDrive/Desktop/Hand_Gesture_Controller/Visual Entry/ArabicHandGesture/ArabicHandGesture/hand-gesture-recognition-mediapipe/model/keypoint_classifier/keypoint.csv"
+model_save_path = "C:/Users/hesha/OneDrive/Desktop/Hand_Gesture_Controller/Visual Entry/ArabicHandGesture/ArabicHandGesture/hand-gesture-recognition-mediapipe/model/keypoint_classifier/keypoint_classifier.hdf5"
+NUM_CLASSES = 5
+X_dataset = np.loadtxt(dataset, delimiter=',', dtype='float32', usecols=list(range(1, (21 * 2) + 1)))
+y_dataset = np.loadtxt(dataset, delimiter=',', dtype='int32', usecols=(0))
+X_train, X_test, y_train, y_test = train_test_split(X_dataset, y_dataset, train_size=0.75, random_state=RANDOM_SEED)
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Input((21 * 2, )),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(20, activation='relu'),
+    tf.keras.layers.Dropout(0.4),
+    tf.keras.layers.Dense(10, activation='relu'),
+    tf.keras.layers.Dense(NUM_CLASSES, activation='softmax')
+])
+
+model.summary()  # tf.keras.utils.plot_model(model, show_shapes=True)
+
+# モデルチェックポイントのコールバック
+model_save_path = "C:/Users/hesha/OneDrive/Desktop/Hand_Gesture_Controller/Visual Entry/ArabicHandGesture/ArabicHandGesture/hand-gesture-recognition-mediapipe/model/keypoint_classifier/keypoint_classifier.keras"
+cp_callback = tf.keras.callbacks.ModelCheckpoint(
+    model_save_path, verbose=1, save_weights_only=False)
+
+# 早期打ち切り用コールバック
+es_callback = tf.keras.callbacks.EarlyStopping(patience=20, verbose=1)
+
+# モデルコンパイル
+model.compile(
+    optimizer='adam',
+    loss='sparse_categorical_crossentropy',
+    metrics=['accuracy']
+)
+
+model.fit(
+    X_train,
+    y_train,
+    epochs=1000,
+    batch_size=128,
+    validation_data=(X_test, y_test),
+    callbacks=[cp_callback, es_callback]
+)
+
+
+# モデル評価
+val_loss, val_acc = model.evaluate(X_test, y_test, batch_size=128)
+
+# 保存したモデルのロード
+model = tf.keras.models.load_model(model_save_path)
+
+# 推論テスト
+predict_result = model.predict(np.array([X_test[0]]))
+print(np.squeeze(predict_result))
+print(np.argmax(np.squeeze(predict_result)))
+
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix, classification_report
+
+
+def print_confusion_matrix(y_true, y_pred, report=True):
+    labels = sorted(list(set(y_true)))
+    cmx_data = confusion_matrix(y_true, y_pred, labels=labels)
+
+    df_cmx = pd.DataFrame(cmx_data, index=labels, columns=labels)
+
+    fig, ax = plt.subplots(figsize=(7, 6))
+    sns.heatmap(df_cmx, annot=True, fmt='g', square=False)
+    ax.set_ylim(len(set(y_true)), 0)
+    plt.show()
+
+    if report:
+        print('Classification Report')
+        print(classification_report(y_test, y_pred))
+
+
+Y_pred = model.predict(X_test)
+y_pred = np.argmax(Y_pred, axis=1)
+
+print_confusion_matrix(y_test, y_pred)
+
+# 推論専用のモデルとして保存
+model.save(model_save_path)
+
+# モデルを変換(量子化)
+tflite_save_path = "C:/Users/hesha/OneDrive/Desktop/Hand_Gesture_Controller/Visual Entry/ArabicHandGesture/ArabicHandGesture/hand-gesture-recognition-mediapipe/model/keypoint_classifier/keypoint_classifier.tflite"
+
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
+converter.optimizations = [tf.lite.Optimize.DEFAULT]
+tflite_quantized_model = converter.convert()
+
+open(tflite_save_path, 'wb').write(tflite_quantized_model)
+
+interpreter = tf.lite.Interpreter(model_path=tflite_save_path)
+interpreter.allocate_tensors()
+
+# 入出力テンソルを取得
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
+interpreter.set_tensor(input_details[0]['index'], np.array([X_test[0]]))
+
+
+# 推論実施
+interpreter.invoke()
+tflite_results = interpreter.get_tensor(output_details[0]['index'])
+
+print(np.squeeze(tflite_results))
+print(np.argmax(np.squeeze(tflite_results)))
